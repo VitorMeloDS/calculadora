b5943f2865c4f1510f5475d96bea8ed32fb81839
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-02-12 18:16:54 -0300

    Atualizando arquivos

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+
+let valor;
+let resultado;
+
+function botao(num){
+    valor = document.calc.visor.value += num;
+}
+
+function reseta(){
+    document.calc.visor.value = " ";
+}
+
+function calcula(){
+    resultado = eval(valor);
+    document.calc.visor.value = resultado.toLocaleString("pt-br");
+}
